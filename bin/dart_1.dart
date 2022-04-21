848228d5462a5263377fe08dab00138ad821f196
aleks007ua <aleks.rozhko@gmail.com>
2022-04-21 19:46:38 +0300

    dart

---
+++ bin/dart_1.dart
@@ -0,0 +1,21 @@
+import 'dart:async';
+
+Stream<int> generator() async* {
+  yield 5;
+  yield 8;
+
+  for (var i = 0; i < 4; i++) {
+    yield i;
+  }
+}
+
+void main() {
+  print('one');
+  final result = generator();
+  print('two');
+  result.listen((event) {
+    print(event);
+  });
+  print('three');
+  print('four');
+}
